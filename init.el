e54f39ec4327cc8408b168e6675212b1d1f30e4a
23oclock <flumine@163.com>
2022-12-05 01:15:48 +0800

    update

--- init.el
+++ init.el
@@ -2,6 +2,7 @@
 			 ("nongnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
 			 ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
 			 ("org" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")))
+;; (setq package-load-list '((use-package)))
 (package-initialize)
 
 ;;; use-package
@@ -9,10 +10,15 @@
   (package-refresh-contents)
   (package-install 'use-package))
 (eval-when-compile (require 'use-package))
-(setq use-package-always-ensure t)
-;; (setq use-package-verbose t)
+(setq use-package-always-ensure t
+      ;; use-package-always-defer t
+      use-package-enable-imenu-support t
+      ;; use-package-verbose t
+      use-package-expand-minimally t)
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(setq default-directory "c:/Users/chuan/")
+
 (require 'init-ui)
 (require 'init-emacs)
 (require 'init-minibuffer)
@@ -20,3 +26,4 @@
 (require 'init-project)
 (require 'init-pyim)
 (require 'init-editor)
+(require 'init-latex)
